87a7f3cb22267cf2b98d8b571f3c9a6bff4f82cd
root <root@node1.vpn.immauss.com>
2019-08-08 20:50:08 +0000

    Initial commit

---
+++ bin/cert-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# script to setup CA and install certs/keys needed for openvpn server 
+#
+yum -y install easy-rsa
+
+mkdir -p /etc/openvpn/easyrsa 
+cp -rp  /usr/share/easy-rsa/3.0.3/* /etc/openvpn/easyrsa/
+cd /etc/openvpn/easyrsa 
+./easyrsa init-pki
+./easyrsa build-ca
+
